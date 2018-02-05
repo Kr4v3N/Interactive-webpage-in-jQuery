f6fa534c154bb8f83412e9bfb1fc4ade13018b4c
Kr4v3N <fafachen37@gmail.com>
2018-02-05 18:38:52 +0100

    Opening and closing

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-// Quand notre page html est prête alors execute la fonction ci-dessous.
-$(document).ready(function(){
-    
-});
-                  
+$(function () {
+    var $mainMenuItems = $("#main-menu ul").children("li"),
+        totalMainMenuItems = $mainMenuItems.lenght,
+        openedIndex = -1,
+        
+        init = function () {
+            
+            $mainMenuItems.children(".images").click(function(){
                 
+                var newIndex = $(this).parent().index(),
+                $item = $mainMenuItems.eq(newIndex),
+                $colorImage = $item.find(".color");
+                
+                $colorImage.animate({left:"0px"},250);
+                $item.animate({width: "420px"},250);
+                openedIndex = newIndex;
+            });
+        };
+    init(); 
+    
+
+});
